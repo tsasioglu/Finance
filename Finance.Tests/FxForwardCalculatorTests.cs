a8c4e4f6c5b1601e96c4f5663b2877c6a9323caf
tsasioglu <tsasioglu@gmail.com>
2015-01-31 21:12:07 +0000

    Added basic forward rate/points calculation

---
+++ Finance.Tests/FxForwardCalculatorTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Finance.Tests
+{
+    [TestFixture]
+    public class FxForwardCalculatorTests
+    {
+        private IFxForwardCalculator _forwardCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _forwardCalculator = new FxForwardCalculator();
+        }
+
+        #region CalculateForwardRate
+
+        [Test]
+        public void CalculateForwardRate_ThrowsException_WhenSpotZero()
+        {
+            const decimal spot = 0m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardRate(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardRate_ThrowsException_WhenSpotNegative()
+        {
+            const decimal spot = -1m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardRate(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardRate_ThrowsException_WhenDaysNegative()
+        {
+            const decimal spot = 1.5m;
+            const int     days = -1;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardRate(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardRate_Calculates_WhenSimple()
+        {
+            const decimal spot = 1.5m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            decimal fowardRate = _forwardCalculator.CalculateForwardRate(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount);
+
+            Assert.That(fowardRate, Is.EqualTo(1.4702457956015523932729624837m));
+        }
+
+        #endregion CalculateForwardRate
+
+        #region CalculateForwardPoints
+
+        [Test]
+        public void CalculateForwardPoints_ThrowsException_WhenSpotZero()
+        {
+            const decimal spot = 0m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount  = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardPoints(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardPoints_ThrowsException_WhenSpotNegative()
+        {
+            const decimal spot = -1m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardPoints(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardPoints_ThrowsException_WhenDaysNegative()
+        {
+            const decimal spot = 1.5m;
+            const int     days = -1;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _forwardCalculator.CalculateForwardPoints(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount));
+        }
+
+        [Test]
+        public void CalculateForwardPoints_Calculates_WhenSimple()
+        {
+            const decimal spot = 1.5m;
+            const int     days = 184;
+            const decimal baseInterestRate   = 0.06m;
+            const decimal quotedInterestRate = 0.02m;
+            const DayCountConvention baseDayCount   = DayCountConvention._360;
+            const DayCountConvention quotedDayCount = DayCountConvention._360;
+
+            decimal fowardRate = _forwardCalculator.CalculateForwardPoints(spot, days, baseInterestRate, baseDayCount, quotedInterestRate, quotedDayCount);
+
+            Assert.That(fowardRate, Is.EqualTo(-0.0000297542043984476067270375M));
+        }
+
+        #endregion CalculateForwardRate
+
+    }
+}
---
+++ Finance/FxForwardCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Finance
+{
+    public interface IFxForwardCalculator
+    {
+        /// <summary>
+        /// Calculates the forward FX rate for the given currencies
+        /// </summary>
+        /// <param name="spot">Spot rate</param>
+        /// <param name="days">Length of contract</param>
+        /// <param name="baseInterestRate">Base currency interest rate</param>
+        /// <param name="baseDayCountConvention">Base currency day count convention</param>
+        /// <param name="quotedInterestRate">Quoted currency interest rate</param>
+        /// <param name="quotedDayCountConvention">Quoted currency day count convention</param>
+        /// <returns>Forward FX Rate</returns>
+        decimal CalculateForwardRate(decimal spot, int days, decimal baseInterestRate, DayCountConvention baseDayCountConvention, decimal quotedInterestRate, DayCountConvention quotedDayCountConvention);
+
+        /// <summary>
+        /// Calculates the forward FX basis points for the given currencies
+        /// </summary>
+        /// <param name="spot">Spot rate</param>
+        /// <param name="days">Length of contract</param>
+        /// <param name="baseInterestRate">Base currency interest rate</param>
+        /// <param name="baseDayCountConvention">Base currency day count convention</param>
+        /// <param name="quotedInterestRate">Quoted currency interest rate</param>
+        /// <param name="quotedDayCountConvention">Quoted currency day count convention</param>
+        /// <returns>Forward Basis Points</returns>
+        decimal CalculateForwardPoints(decimal spot, int days, decimal baseInterestRate, DayCountConvention baseDayCountConvention, decimal quotedInterestRate, DayCountConvention quotedDayCountConvention);
+    }
+
+    public class FxForwardCalculator : IFxForwardCalculator
+    {
+        /// <summary>
+        /// Calculates the forward FX rate for the given currencies
+        /// </summary>
+        /// <param name="spot">Spot rate</param>
+        /// <param name="days">Length of contract</param>
+        /// <param name="baseInterestRate">Base currency interest rate</param>
+        /// <param name="baseDayCountConvention">Base currency day count convention</param>
+        /// <param name="quotedInterestRate">Quoted currency interest rate</param>
+        /// <param name="quotedDayCountConvention">Quoted currency day count convention</param>
+        /// <returns>Forward FX Rate</returns>
+        public decimal CalculateForwardRate(decimal spot, int days, decimal baseInterestRate, DayCountConvention baseDayCountConvention, decimal quotedInterestRate, DayCountConvention quotedDayCountConvention)
+        {
+            if (spot <= 0) throw new ArgumentOutOfRangeException("spot", "Must not be null");
+            if (days < 0)  throw new ArgumentOutOfRangeException("days", "Must not be null");
+
+            decimal quotedEffectiveRate = quotedInterestRate * days / (int)quotedDayCountConvention;
+            decimal baseEffectiveRate   = baseInterestRate   * days / (int)baseDayCountConvention;
+
+            return spot * (1 + quotedEffectiveRate)/(1 + baseEffectiveRate);
+        }
+
+        /// <summary>
+        /// Calculates the forward FX basis points for the given currencies
+        /// </summary>
+        /// <param name="spot">Spot rate</param>
+        /// <param name="days">Length of contract</param>
+        /// <param name="baseInterestRate">Base currency interest rate</param>
+        /// <param name="baseDayCountConvention">Base currency day count convention</param>
+        /// <param name="quotedInterestRate">Quoted currency interest rate</param>
+        /// <param name="quotedDayCountConvention">Quoted currency day count convention</param>
+        /// <returns>Forward Basis Points</returns>
+        public decimal CalculateForwardPoints(decimal spot, int days, decimal baseInterestRate, DayCountConvention baseDayCountConvention, decimal quotedInterestRate, DayCountConvention quotedDayCountConvention)
+        {
+            decimal forwardRate = CalculateForwardRate(spot, days, baseInterestRate, baseDayCountConvention,quotedInterestRate, quotedDayCountConvention);
+            return (forwardRate - spot) / 1000;
+        }
+    }
+
+    public enum DayCountConvention
+    {
+        _360 = 360,
+        _365 = 365,
+    }
+}
