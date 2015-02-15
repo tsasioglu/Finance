591477e65e647cc86449168f580fd8b6f0e47acd
tsasioglu <tsasioglu@gmail.com>
2015-02-15 17:06:28 +0000

    Added straight line linear and logarithmic interpolation

---
+++ Finance.Tests/Calculators/PolationCalculatorTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Finance.Calculators;
+using NUnit.Framework;
+
+namespace Finance.Tests.Calculators
+{
+    [TestFixture]
+    public class PolationCalculatorTests
+    {
+        private PolationCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new PolationCalculator();
+        }
+
+        #region StraightLineIntepolate
+
+        [Test]
+        public void StraightLineIntepolate_Calculates_WhenSameDays()
+        {
+            const decimal firstDays  = 30m;
+            const decimal firstRate  = 0.08m;
+            const decimal secondDays = 30m;
+            const decimal secondRate = 0.085m;
+            const decimal targetDays = 39m;
+
+            Assert.Throws<ArgumentException>(() => _calculator.StraightLineIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays));
+        }
+
+        [Test]
+        public void StraightLineIntepolate_Calculates_WhenSameRate()
+        {
+            const decimal firstDays  = 30m;
+            const decimal firstRate  = 0.08m;
+            const decimal secondDays = 61m;
+            const decimal secondRate = 0.08m;
+            const decimal targetDays = 39m;
+
+            decimal targetRate = _calculator.StraightLineIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays);
+
+            Assert.That(targetRate, Is.EqualTo(0.08m));
+        }
+
+        [Test]
+        public void StraightLineIntepolate_Calculates_WhenSimple()
+        {
+            const decimal firstDays  = 30m;
+            const decimal firstRate  = 0.08m;
+            const decimal secondDays = 61m;
+            const decimal secondRate = 0.085m;
+            const decimal targetDays = 39m;
+
+            decimal targetRate = _calculator.StraightLineIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays);
+
+            Assert.That(targetRate, Is.EqualTo(0.0814516129032258064516129034m));
+        }
+
+        #endregion StraightLineIntepolate
+
+        #region LogarithmicIntepolate
+
+        [Test]
+        public void LogarithmicIntepolate_Calculates_WhenSameDays()
+        {
+            const decimal firstDays  = 30m;
+            const decimal firstRate  = 0.08m;
+            const decimal secondDays = 30m;
+            const decimal secondRate = 0.085m;
+            const decimal targetDays = 39m;
+
+            Assert.Throws<ArgumentException>(() => _calculator.LogarithmicIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays));
+        }
+
+        [Test]
+        public void LogarithmicIntepolate_Calculates_WhenSameRate()
+        {
+            const decimal firstDays  = 30m;
+            const decimal firstRate  = 0.08m;
+            const decimal secondDays = 61m;
+            const decimal secondRate = 0.08m;
+            const decimal targetDays = 39m;
+
+            decimal targetRate = _calculator.LogarithmicIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays);
+
+            Assert.That(targetRate, Is.EqualTo(0.08m));
+        }
+
+        [Test]
+        public void LogarithmicIntepolate_Calculates_WhenSimple()
+        {
+            const decimal firstDays  = 61m;
+            const decimal firstRate  = 0.075m;
+            const decimal secondDays = 92m;
+            const decimal secondRate = 0.076m;
+            const decimal targetDays = 73m;
+
+            decimal targetRate = _calculator.LogarithmicIntepolate(firstDays, firstRate, secondDays, secondRate, targetDays);
+
+            Assert.That(targetRate, Is.EqualTo(7.53855263288879m));
+        }
+
+        #endregion LogarithmicIntepolate
+    }
+}
---
+++ Finance/Calculators/PolationCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Finance.Calculators
+{
+    public class PolationCalculator
+    {
+        /// <summary>
+        /// Uses straight line interpolation to calculate the estimated value between two points.
+        /// </summary>
+        /// <param name="x1">First known value in the first dimension</param>
+        /// <param name="y1">First known value in the second dimention</param>
+        /// <param name="x2">Second known value in the first dimension</param>
+        /// <param name="y2">Second known value in the second dimension</param>
+        /// <param name="xTarget">Value between x1 and x2 in the first dimension for which we wish to find the corresponding value in the second dimension</param>
+        /// <returns>Value between y1 and y2 in the second dimension which corresponds to xTarget</returns>
+        public decimal StraightLineIntepolate(decimal x1, decimal y1, decimal x2, decimal y2, decimal xTarget)
+        {
+            if (x1 == x2)
+                throw new ArgumentException(String.Format("Distinct reference points required. Only '{0}' provided. ", x1));
+            if (y1 == y2)
+                return y1;
+
+            decimal gradient   = (y2 - y1)/(x2 - x1);
+            decimal additional = (xTarget - x1);
+
+            return y1 + gradient * additional;
+        }
+
+        /// <summary>
+        /// Uses logarithmic (i.e. geometric) interpolation to calculate the estimated value between two points.
+        /// </summary>
+        /// <param name="x1">First known value in the first dimension</param>
+        /// <param name="y1">First known value in the second dimention</param>
+        /// <param name="x2">Second known value in the first dimension</param>
+        /// <param name="y2">Second known value in the second dimension</param>
+        /// <param name="xTarget">Value between x1 and x2 in the first dimension for which we wish to find the corresponding value in the second dimension</param>
+        /// <returns>Value between y1 and y2 in the second dimension which corresponds to xTarget</returns>
+        public decimal LogarithmicIntepolate(decimal x1, decimal y1, decimal x2, decimal y2, decimal xTarget)
+        {
+            if (x1 == x2)
+                throw new ArgumentException(String.Format("Distinct reference points required. Only '{0}' provided. ", x1));
+            if (y1 == y2)
+                return y1;
+
+            decimal gradient  = (xTarget - x1) / (x2 - x1);
+            double additional = (Math.Log((double) y2 *100) - Math.Log((double) y1 * 100));
+            var power         = (Math.Log((double) y1 * 100) + (double) (gradient * (decimal) additional));
+
+            return (decimal) Math.Pow(Math.E, power); 
+        }
+    }
+}
