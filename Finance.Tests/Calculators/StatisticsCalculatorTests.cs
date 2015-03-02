1002a017bc86be74f1e6a1f63576c6ec4ef83f36
tsasioglu <tsasioglu@gmail.com>
2015-03-02 20:39:57 +0000

    Added stats calculator (sd and var)

---
+++ Finance.Tests/Calculators/StatisticsCalculatorTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Finance.Calculators;
+using NUnit.Framework;
+
+namespace Finance.Tests.Calculators
+{
+    public class StatisticsCalculatorTests
+    {
+        private StatisticsCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new StatisticsCalculator();
+        }
+
+        #region CalculateVariance Tests
+
+        [Test]
+        public void CalculateVariance_ThrowsException_WhenNullInputs()
+        {
+            IEnumerable<decimal> inputs = null;
+
+            Assert.Throws<ArgumentNullException>(() => _calculator.CalculateVariance(inputs));
+        }
+
+        [Test]
+        public void CalculateVariance_ThrowsException_WhenEmptyInputs()
+        {
+            IEnumerable<decimal> inputs = Enumerable.Empty<decimal>();
+
+            Assert.Throws<ArgumentException>(() => _calculator.CalculateVariance(inputs));
+        }
+
+        [Test]
+        public void CalculateVariance_ReturnsZero_WhenSingleInput()
+        {
+            IEnumerable<decimal> inputs = new[] { 4m };
+
+            decimal variance = _calculator.CalculateVariance(inputs);
+
+            Assert.That(variance, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateVariance_ReturnsCorrect_WhenSimple()
+        {
+            IEnumerable<decimal> inputs = new[] { 83m, 87m, 82m, 89m, 88m };
+
+            decimal variance = _calculator.CalculateVariance(inputs);
+
+            Assert.That(variance, Is.EqualTo(7.76m));
+        }
+
+        [Test]
+        public void CalculateVariance_ReturnsCorrect_WhenSimple_Samples()
+        {
+            IEnumerable<decimal> inputs = new[] { 83m, 87m, 82m, 89m, 88m };
+            bool sample = true;
+
+            decimal variance = _calculator.CalculateVariance(inputs, sample);
+
+            Assert.That(variance, Is.EqualTo(9.7m));
+        }
+
+        #endregion CalculateVariance Tests
+
+
+        #region CalculateStandardDeviation Tests
+
+        [Test]
+        public void CalculateStandardDeviation_ThrowsException_WhenNullInputs()
+        {
+            IEnumerable<decimal> inputs = null;
+
+            Assert.Throws<ArgumentNullException>(() => _calculator.CalculateStandardDeviation(inputs));
+        }
+
+        [Test]
+        public void CalculateStandardDeviation_ThrowsException_WhenEmptyInputs()
+        {
+            IEnumerable<decimal> inputs = Enumerable.Empty<decimal>();
+
+            Assert.Throws<ArgumentException>(() => _calculator.CalculateStandardDeviation(inputs));
+        }
+
+        [Test]
+        public void CalculateStandardDeviation_ReturnsZero_WhenSingleInput()
+        {
+            IEnumerable<decimal> inputs = new[] { 4m };
+
+            decimal variance = _calculator.CalculateStandardDeviation(inputs);
+
+            Assert.That(variance, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateStandardDeviation_ReturnsCorrect_WhenSimple()
+        {
+            IEnumerable<decimal> inputs = new[] { 83m, 87m, 82m, 89m, 88m };
+
+            decimal variance = _calculator.CalculateStandardDeviation(inputs);
+
+            Assert.That(variance, Is.EqualTo(2.78567765543682m));
+        }
+
+        [Test]
+        public void CalculateStandardDeviation_ReturnsCorrect_WhenSimple_Samples()
+        {
+            IEnumerable<decimal> inputs = new[] { 83m, 87m, 82m, 89m, 88m };
+            bool sample = true;
+
+            decimal variance = _calculator.CalculateStandardDeviation(inputs, sample);
+
+            Assert.That(variance, Is.EqualTo(3.11448230047949m));
+        }
+
+        #endregion CalculateStandardDeviation Tests
+    }
+}
---
+++ Finance/Calculators/StatisticsCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Finance.Calculators
+{
+    public class StatisticsCalculator
+    {
+        /// <summary>
+        ///  Calculates the Variance of the set of numbers given
+        /// </summary>
+        /// <param name="values">Set of values to calculate the variance of</param>
+        /// <param name="sample">True if values is a sample, False if values is the entire population</param>
+        /// <returns>Variance</returns>
+        public decimal CalculateVariance(IEnumerable<decimal> values, bool sample = false)
+        {
+            if (values == null) throw new ArgumentNullException("values", "Must not be null. ");
+
+            decimal[] inputs = values.ToArray();
+
+            if (!inputs.Any()) throw new ArgumentException("Must not be empty. ", "values");
+
+
+            decimal mean = inputs.Average();
+            double sumOfDifferences = inputs.Sum(x => Math.Pow((double) (mean - x), 2));
+            int numberOfValuesDivisor = sample ? inputs.Length - 1 : inputs.Length;
+            return (decimal) (sumOfDifferences / numberOfValuesDivisor);
+        }
+
+        /// <summary>
+        ///  Calculates the Standard Deviation (sqrt. Variance) of the set of numbers given
+        /// </summary>
+        /// <param name="values">Set of values to calculate the variance of</param>
+        /// <param name="sample">True if values is a sample, False if values is the entire population</param>
+        /// <returns>Standard Deviation (sqrt. Variance)</returns>
+        public decimal CalculateStandardDeviation(IEnumerable<decimal> values, bool sample = false)
+        {
+            return (decimal) Math.Sqrt((double) CalculateVariance(values, sample));
+        }
+    }
+}
